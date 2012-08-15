c1c2286ceb07b2be36e7d08925804963190fd278
D. Ryan Hild <d.ryan.hild@gmail.com>
2012-08-15 15:55:50 -0600

    Added message to breakpoint interrupt.
    Simple message of 'Breakpoint detected!' now should print upon
    recieving a breakpoint exception.

--- src/interrupts/breakpoint_exception.asm
+++ src/interrupts/breakpoint_exception.asm
@@ -1,5 +1,14 @@
 global breakpoint_exception
+extern print_string
 bits 64
+section .text
 
 breakpoint_exception:
+	mov rdi, message
+	call print_string
 	iretq
+
+section .data
+
+message:
+	db 'Breakpoint detected!', 0
