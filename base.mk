86e02defd3db69bf1cfcc99c57065919e90fe40e
D. Ryan Hild <d.ryan.hild@gmail.com>
2012-08-16 20:12:02 -0600

    Changed flags for nasm and gcc.
    Position-independent code isn't as important, and debugging symbols from
    nasm don't relocate to the full 64-bits =(.

--- base.mk
+++ base.mk
@@ -13,10 +13,10 @@ UMOUNT = $(SUDO) umount
 WARNING_FLAGS = -Wall -Wextra -Werror
 STANDALONE_FLAGS = -nostdlib -fno-builtin -nostartfiles -nodefaultlibs -ffreestanding
 CONVENTION_FLAGS = -fno-exceptions -fno-stack-protector -mno-red-zone
-MODE_FLAGS = -m64 -mcmodel=large -fPIC -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow
+MODE_FLAGS = -m64 -mcmodel=large -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow
 CFLAGS = $(WARNING_FLAGS) $(STANDALONE_FLAGS) $(CONVENTION_FLAGS) $(MODE_FLAGS) -g
 CXXFLAGS = $(CFLAGS) -fno-rtti
-NASMFLAGS = -f elf64 -g
+NASMFLAGS = -f elf64
 LDFLAGS = -melf_x86_64 -nostdlib -nodefaultlibs -z max-page-size=0x1000
 
 .SUFFIXES:
