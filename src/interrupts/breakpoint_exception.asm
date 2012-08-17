b391140c2aaeb2e1bb2baf8e2207e7243a3056ca
D. Ryan Hild <d.ryan.hild@gmail.com>
2012-08-17 13:32:11 -0600

    Added interrupt code to store register values for inspection & print them.
    Interrupt Makefile also now properly detects changes in source files.

--- src/interrupts/breakpoint_exception.asm
+++ src/interrupts/breakpoint_exception.asm
@@ -1,11 +1,15 @@
 global breakpoint_exception
 extern print_string
+extern store_print_regs
 bits 64
 section .text
 
 breakpoint_exception:
+	call store_print_regs
+
 	mov rdi, message
 	call print_string
+
 	iretq
 
 section .data
