fc149c70a2af3a5aeec1a4514f89d59599958520
D. Ryan Hild <d.ryan.hild@gmail.com>
2012-08-18 00:39:29 -0600

    Reorganized the buddy allocator code.
    It is a lot closer to being complete, but there are still some holes.

--- src/memory/buddyAllocator.cpp
+++ src/memory/buddyAllocator.cpp
@@ -1,11 +1,109 @@
-#include "memory.h"
+#include "buddyAllocator.h"
 #include "kprintf.h"
 
 using namespace buddy;
 
-Block* Block::blocks[BUDDY_LEVELS];
-qword Block::blockCounts[BUDDY_LEVELS];
-qword Block::blockSizes[BUDDY_LEVELS];
+BlockManager::BlockManager(void* const loc, const qword length) : start(loc), size(length) {
+  // First, allocate enough arrays to store our blocks.
+  qword remaining = size;
+  void* pos = loc;
+  for (block_order_t i = BUDDY_LEVELS - 1; i >= 0; i--) {
+    counts[i] = remaining / Block::getSize(i);
+    blocks[i] = (Block*)(pos);
+    remaining -= counts[i] * sizeof(Block);
+    pos = (void*)((qword)pos + counts[i] * sizeof(Block));
+  }
+
+  for (block_order_t i = 0; i < BUDDY_LEVELS; i++) {
+    Block* b = blocks[i];
+    const qword blockSize = Block::getSize(i);
+    counts[i] = remaining / blockSize;
+    for (qword j = 0; j < counts[i]; j++) {
+      b = new(b) Block(pos, i);
+      pos = (void*)((qword)pos + blockSize);
+      b++;
+    }
+  }
+}
+
+BlockManager::~BlockManager() {
+  for (block_order_t i = 0; i < BUDDY_LEVELS; i++) {
+    Block* b = blocks[i];
+    for (qword j = 0; j < counts[i]; j++) {
+      b->Block::~Block();
+      b++;
+    }
+  }
+}
+
+void* BlockManager::allocate(qword size) {
+  block_order_t order = 0;
+  while ((Block::getSize(order) < size) && (order < BUDDY_LEVELS))
+    order++;
+
+  if (order < BUDDY_LEVELS) {
+    // The requested size fits within a single block.
+    // Search for an available block.
+    Block* b = blocks[order];
+    for (qword i = 0; i < counts[order]; i++) {
+      if (!b->isUsed()) {
+	b->setUsed(true);
+	return b->getLocation();
+      }
+    }
+    log_error("Unable to allocate block!");
+    return NULL;
+  } else {
+    // We need several blocks.
+    qword blocksNeeded = (size - 1 + Block::maxSize()) / Block::maxSize();
+    Block* start = blocks[BUDDY_LEVELS - 1];
+    for (qword i = 0; i <= (counts[BUDDY_LEVELS - 1] - blocksNeeded); i++) {
+      bool stillGood = true;
+      Block* b = start;
+      for (qword j = 0; j < blocksNeeded; j++) {
+	if (b->isUsed()) {
+	  stillGood = false;
+	  break;
+	}
+	b++;
+      }
+      if (stillGood) {
+	// Awesome! We have the needed blocks!
+	void* mem = start->getLocation();
+	start->setUsed(true);
+	b = start;
+	b++;
+	for (qword j = 0; j < blocksNeeded; j++) {
+	  b->setUsed(true);
+	  start++;
+	  b++;
+	}
+	return mem;
+      }
+      start++;
+    }
+  }
+  log_error("Failed to allocate memory!");
+  return NULL;
+}
+
+void BlockManager::checkMerge(Block* b) {
+  if (b == NULL)
+    return;
+
+  if (b->isUsed())
+    return;
+
+  if (b->isMaxOrder())
+    return; // No recombination possible.
+
+  // We're good, check the buddy block.
+  //const qword bIndex = ((qword)(b->getLocation()) - (qword)(this->start)) / b->getSize();
+}
+
+void BlockManager::free(void*) {
+}
+
 bool Block::initialized = false;
 
 void Block::initialize() {
@@ -14,8 +112,29 @@ void Block::initialize() {
     blockSizes[i] = size;
     size *= 2;
   }
-  blockCount = 0;
   initialized = true;
+}
+
+Block::Block(void* loc, const block_order_t o)
+  : location(loc), order(o), usage(FREE) {}
+
+Block::~Block() {}
+
+void Block::split(Block* a, Block* b) {
+  const void* low = this->location;
+  const void* high = (void*)((qword)low + this->getSize());
+  if ((a->location < low) || (a->location > high) ||
+      (b->location < low) || (b->location > high))
+    return;
+
+  this->usage = SPLIT;
+}
+
+qword Block::getSize() {
+  qword size = BUDDY_SMALLEST_BLOCK;
+  for (block_order_t i = 0; i < BUDDY_LEVELS; i++)
+    size *= 2;
+  return size;
 }
 
 qword Block::getSize(const block_order_t s) {
@@ -30,168 +149,4 @@ qword Block::getSize(const block_order_t s) {
     return 0;
   }
   return blockSizes[s];
-}
-
-Block::Block(const void* loc, const block_order_t o) : location(loc), order(o), inUse(false) {}
-
-void Block::split() {
-  if (this->order == 0) {
-    log_error( "Trying to split lowest order block!" );
-    return;
-  }
-  if (this->isUsed()) {
-    log_error( "Trying to split block in use!" );
-    return;
-  }
-  this->setUsed( true );
-
-  block_order_t order = this->order - 1;
-  Block* childA = new Block(this->location, order );
-  Block* childB = new Block(this->location + getSize(order), order );
-}
-
-Block* Block::getFree(const block_order_t order) {
-  for (qword i = 0; i < blockCounts[order]; i++) {
-    if (blocks[order][i]->inUse == false) {
-      blocks[order][i]->inUse = true;
-      return blocks[order][i];
-    }
-  }
-  if (order == BUDDY_LEVELS) {
-    log_error( "Unable to allocate largest block!" );
-    return NULL;
-  }
-
-  Block* bigger = getFree(order + 1);
-  if (bigger == NULL) {
-    log_error( "Unable to allocate larger block!" );
-    return NULL;
-  }
-  split_block( bigger );
-
-  if (blocks[blockCounts[order] - 2]->inUse == false) {
-    blocks[blockCounts[order] - 2]->inUse = true;
-    return blocks[blockCounts[order] - 2];
-  }
-  if (blocks[blockCounts[order] - 1]->inUse == false) {
-    blocks[blockCounts[order] - 1]->inUse = true;
-    return blocks[blockCounts[order] - 1];
-  }
-
-  log_error( "Unable to find a free block!" );
-  return NULL;
-}
-
-Block* findByLoc(const void* loc) {
-  for (block_order_t i = BUDDY_LEVELS - 1; i > -1; i--) {
-    for (qword j = 0; j < blockCounts[i]; j++) {
-      if (blocks[i][j]->location == location)
-	return blocks[i][j];
-    }
-  }
-  return NULL;
-}
-
-void* Block::requestMemory(qword size) {
-  if (!initialized)
-    initialize();
-
-  for (block_order_t i = 0; i < BUDDY_LEVELS; i++) {
-    if (size < blockSizes[i]) {
-      BuddyBlock* block = getFree(i);
-      if (block == NULL) {
-	log_error( "Unable to fill request for memory!" );
-	return NULL;
-      }
-      block->setUsed(true);
-      return block->location;
-    }
-  }
-  // We need more than one of the largest blocks.
-  // For now, return several of the largest blocks.
-  // This is a waste, especially if it's a request for only a few
-  // more bytes, but for now we'll have to just deal with it.
-  // TODO: Fix physical block allocator to use smallest number of blocks possible.
-  qword request_count = (size + BLOCK_MAX_SIZE - 1) / BLOCK_MAX_SIZE;
-  BuddyBlock* found_blocks[request_count];
-  for (qword i = 0; i < block_counts[BUDDY_LEVELS - 1]; i++) {
-    found_blocks[0] = blocks[BUDDY_LEVELS - 1][i];
-    if (found_blocks[0]->inUse)
-      continue;
-    for (qword j = 1; j < request_count; j++) {
-      found_blocks[j] = get_specific_block( location );
-      if ((found_blocks[j]->inUse) || (found_blocks[j]->order != (BUDDY_LEVELS - 1)))
-	continue;
-    }
-    // We have enough blocks!
-    mark_blocks_in_use( found_blocks, request_count );
-    return found_blocks[0]->location;
-  }
-  // We couldn't satisfy the request!
-  log_error( "Unable to fill request for memory! Not enough contiguous blocks available" );
-  return NULL;
-}
-
-/** Requests a given size of memory from the buddy allocator.
- */
-void* buddy_request_memory( qword size ) {
-  buddy_check_initialized();
-  for (block_order_t i = 0; i < BUDDY_LEVELS; i++) {
-    if (size < block_sizes[i]) {
-      BuddyBlock* block = get_block( i );
-      if (block == NULL) {
-	log_error( "Unable to fill request for memory!" );
-	return NULL;
-      }
-      mark_blocks_in_use( &block, 1 );
-      return block->location;
-    }
-  }
-  // We need more than one of the largest blocks.
-  // For now, return several of the largest blocks.
-  // This is a waste, especially if it's a request for only a few
-  // more bytes, but for now we'll have to just deal with it.
-  // TODO: Fix physical block allocator to use smallest number of blocks possible.
-  qword request_count = (size + BLOCK_MAX_SIZE - 1) / BLOCK_MAX_SIZE;
-  BuddyBlock* found_blocks[request_count];
-  for (qword i = 0; i < block_counts[BUDDY_LEVELS - 1]; i++) {
-    found_blocks[0] = blocks[BUDDY_LEVELS - 1][i];
-    if (found_blocks[0]->inUse)
-      continue;
-    for (qword j = 1; j < request_count; j++) {
-      found_blocks[j] = get_specific_block( location );
-      if ((found_blocks[j]->inUse) || (found_blocks[j]->order != (BUDDY_LEVELS - 1)))
-	continue;
-    }
-    // We have enough blocks!
-    mark_blocks_in_use( found_blocks, request_count );
-    return found_blocks[0]->location;
-  }
-  // We couldn't satisfy the request!
-  log_error( "Unable to fill request for memory! Not enough contiguous blocks available" );
-  return NULL;
-}
-
-/** Requests to release the memory pointed at by the given address.
- */
-void buddy_release_memory(const void* mem) {
-  buddy_check_initialized(); // Really, we should be initialized here, but check anyways.
-  BuddyBlock* block = get_specific_block( mem );
-  if (block == NULL) {
-    log_error( "Attempting to free a memory address that can't be found as a block!" );
-    return;
-  }
-  block->inUse = false;
-}
-
-/** Calls the buddy allocator.
- */
-void* request_memory( qword size ) {
-  return buddy_request_memory( size );
-}
-
-/** Calls the buddy deallocator.
- */
-void release_memory( const void* mem ) {
-  buddy_release_memory( mem );
 }
--- src/memory/buddyAllocator.h
+++ src/memory/buddyAllocator.h
@@ -1,6 +1,8 @@
 #ifndef BUDDY_ALLOCATOR_H
 #define BUDDY_ALLOCATOR_H
 
+#include "types.h"
+
 // Smallest block size is X bytes
 #define BUDDY_SMALLEST_BLOCK 4096
 // There are X levels of allocation, each twice as big as the last
@@ -8,40 +10,60 @@
 
 namespace buddy {
 
-  typedef block_order_t sbyte;
+  typedef sbyte block_order_t;
+
+  enum BlockUsage {
+    FREE,
+    INUSE,
+    SPLIT
+  };
 
   class Block {
   private:
     Block(const Block&);
-    static Block* blocks;
-    static qword blockCount;
     static qword* blockSizes;
     static bool initialized;
     static void initialize();
 
-    static Block* getFree(const block_order_t);
-    static Block* findByLoc(const void*);
-
-    void split();
-
-    void* location;
-    block_order_t order;
-    bool inUse;
+    void * const location;
+    const block_order_t order;
+    BlockUsage usage;
 
   public:
-    Block(const void*, const block_order_t);
+    Block(void*, const block_order_t);
     ~Block();
-    void setUsed(const bool b) { this->inUse = b; }
-    bool isUsed() { return this->inUse; }
+    void setUsed(const bool inUse) { this->usage = inUse ? INUSE : FREE; }
+    bool isUsed() { return (this->usage != FREE); }
+    BlockUsage getUsage() { return usage; }
+    void* getLocation() { return location; }
+    block_order_t getOrder() { return order; }
+    bool isMaxOrder() { return (order + 1) == BUDDY_LEVELS; }
 
+    void split(Block*, Block*);
+
+    qword getSize();
     static qword getSize(const block_order_t);
     static qword maxSize() { return getSize(BUDDY_LEVELS - 1); }
 
-    void* operator new(qword);
+    void* operator new(qword, void*);
     void operator delete(void*);
+  };
 
-    static void* requestMemory(qword);
-    static void releaseMemory(void*);
+  class BlockManager {
+  private:
+    void* const start;
+    const qword size;
+    
+    Block* blocks[BUDDY_LEVELS];
+    qword counts[BUDDY_LEVELS];
+
+    void checkMerge(Block*);
+  public:
+    BlockManager(void* const location, const qword size);
+    ~BlockManager();
+
+    void* allocate(qword);
+    void free(void*);
   };
 
 }
