aaa508e9194175dc84ca3ce268c1471930d6748f
D. Ryan Hild <dhild@mymail.mines.edu>
2012-08-28 10:53:27 -0600

    Moved allocate/free interface code to a header.

--- src/memory/memory.h
+++ src/memory/memory.h
@@ -1,10 +1,11 @@
 #ifndef MEMORY_MANAGEMENT_H
 #define MEMORY_MANAGEMENT_H MEMORY_MANAGEMENT_H
 
+#include "paging.h"
 #include "types.h"
 
 // Requests a given size of paged virtual memory.
-void* allocate(const size_t);
-void free(const void*);
+void* allocate(const size_t size) { return paging::allocate(size); }
+void free(const void* loc) { return paging::free(loc); }
 
 #endif
