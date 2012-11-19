f8ad63e22b843d68daa8bc0437ac12bc415940da
D. Ryan Hild <dhild@mymail.mines.edu>
2012-11-19 16:22:20 -0700

    Commit of other files changed in previous update.

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 MAKEFLAGS += -rR
 
+TOS_INCLUDE = $(realpath ./include)
 TOS_BASEMAKE = $(realpath ./base.mk)
 TOS_DEPCHECK = $(realpath ./depcheck.mk)
 
-export TOS_DEPCHECK TOS_BASEMAKE
+export TOS_DEPCHECK TOS_BASEMAKE TOS_INCLUDE
 
 .PHONY: all
+.PHONY: boot include
 .PHONY: src
 .PHONY: images
-.PHONY: umount
 .PHONY: compressed-img
 .PHONY: clean
 .PHONY: image-clean
@@ -18,13 +19,16 @@ all: images
 
 include $(TOS_BASEMAKE)
 
+boot:
+	make -C boot
+
 src:
 	make -C src
 
 hd.img: hd.img.xz
 	xz -dfk hd.img.xz
 
-images: src hd.img
+images: boot src hd.img
 	dd if=hd.img of=part1.img bs=1024 skip=1024 count=69536
 	debugfs -w -f copy_image.debugfs
 	dd if=part1.img of=hd.img bs=1024 seek=1024 count=69536
@@ -34,6 +38,7 @@ compressed-img:
 	xz -zfk hd.img
 
 clean:
+	make -C boot clean
 	make -C src clean
 
 image-clean: compressed-img clean
--- base.mk
+++ base.mk
@@ -1,9 +1,10 @@
 all: .depcheck
 
 .SUFFIXES:
-.SUFFIXES: .c .cpp .asm .o .d
+.SUFFIXES: .c .cpp .asm .o .d .ld .ld.S
 
-CROSS_COMPILE = x86_64-elf-
+CROSS_COMPILE := x86_64-elf-
+TOS_CPP = $(CROSS_COMPILE)cpp
 TOS_CC = $(CROSS_COMPILE)gcc
 TOS_CXX = $(CROSS_COMPILE)g++
 TOS_LD = $(CROSS_COMPILE)ld
@@ -11,19 +12,26 @@ TOS_AR = $(CROSS_COMPILE)ar
 TOS_RANLIB = $(CROSS_COMPILE)ranlib
 TOS_NASM = nasm
 
-WARNING_FLAGS = -Wall -Wextra -Werror
-STANDALONE_FLAGS = -nostdlib -fno-builtin -nostartfiles -nodefaultlibs -ffreestanding
-CONVENTION_FLAGS = -fno-exceptions -fno-stack-protector -mno-red-zone
-MODE_FLAGS = -m64 -mcmodel=large -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow
-CFLAGS += $(WARNING_FLAGS) $(STANDALONE_FLAGS) $(CONVENTION_FLAGS) $(MODE_FLAGS) -g
+WARNING_FLAGS := -Wall -Wextra -Werror -pedantic
+STANDALONE_FLAGS := -nostdlib -fno-builtin -nostartfiles -nodefaultlibs -ffreestanding
+CONVENTION_FLAGS := -fno-exceptions -fno-stack-protector -mno-red-zone
+MODE_FLAGS := -m64 -mcmodel=large -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow
+CPPFLAGS += $(WARNING_FLAGS)
+CPPFLAGS += -I $(TOS_INCLUDE)
+CFLAGS += $(STANDALONE_FLAGS) $(CONVENTION_FLAGS) $(MODE_FLAGS) -g
 CXXFLAGS += $(CFLAGS) -fno-rtti
-NASMFLAGS += -f elf64 -Werror
-LDFLAGS += -melf_x86_64 -nostdlib -nodefaultlibs -z max-page-size=0x1000
+NASMARCH = elf64
+NASMFLAGS += -f $(NASMARCH) -Werror
+LDARCH = elf_x86_64
+LDFLAGS += -m$(LDARCH) -nostdlib -nodefaultlibs -z max-page-size=0x1000
+DEPSFLAGS = -MMD -MP -MF $@.d
 
-cmd_cc_o_c = $(TOS_CC) $(CFLAGS) -MMD -MP -MF $@.d -c -o $@ $<
-cmd_cxx_o_cpp = $(TOS_CXX) $(CXXFLAGS) -MMD -MP -MF $@.d -c -o $@ $<
+cmd_cc_o_c = $(TOS_CC) $(CPPFLAGS) $(CFLAGS) $(DEPSFLAGS) -c -o $@ $<
+cmd_cxx_o_cpp = $(TOS_CXX) $(CPPFLAGS) $(CXXFLAGS) $(DEPSFLAGS) -c -o $@ $<
 cmd_nasm_d_asm = $(TOS_NASM) $(NASMFLAGS) -M $< > $@.d
 cmd_nasm_o_asm = $(TOS_NASM) $(NASMFLAGS) -o $@ $<
+cmd_cpp_ld_ldS = $(TOS_CPP) $(CPPFLAGS) -P -D__ASSEMBLY__ -DLINKERSCRIPT \
+                  $(DEPSFLAGS) -o $@ $<
 
 %.o: %.c
 	$(cmd_cc_o_c)
@@ -34,6 +42,9 @@ cmd_nasm_o_asm = $(TOS_NASM) $(NASMFLAGS) -o $@ $<
 %.o: %.asm
 	$(cmd_nasm_d_asm)
 	$(cmd_nasm_o_asm)
+
+%.ld: %.ld.S
+	$(cmd_cpp_ld_ldS)
 
 include $(TOS_DEPCHECK)
 
