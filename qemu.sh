a8e94b02475cbc2ce772986235cdb49c8c6348ad
Ryan Hild <d.ryan.hild@gmail.com>
2016-03-30 18:52:40 -0600

    Added automatic download of OVMF.fd bios image

--- qemu.sh
+++ qemu.sh
@@ -2,7 +2,17 @@
 
 KERNEL_FILE=build/kernel/kernel.elf
 
+OVMF_ZIP=http://downloads.sourceforge.net/project/edk2/OVMF/OVMF-X64-r15214.zip
+OVMF_BINARY=OVMF.fd
+
+if [ ! -f "$OVMF_BINARY" ]; then
+  echo "Downloading UEFI bios image OVMF.fd"
+  wget "$OVMF_ZIP" -O ovmf.zip
+  unzip ovmf.zip $OVMF_BINARY
+  rm ovmf.zip
+fi
+
 sudo cp $KERNEL_FILE sysroot/boot/kernel.bin
 sudo sync
-qemu-system-x86_64 -s -bios ../seabios.bin -drive file=disk.img,format=raw -m 2048
+qemu-system-x86_64 -s -bios "$OVMF_BINARY" -drive file=disk.img,format=raw -m 2048
 
